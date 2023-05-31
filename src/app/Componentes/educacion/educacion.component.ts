0f400628dbe9cf2b0f367f270ffa876660c1e25a
GastonJava <gastonmirandajava@gmail.com>
2023-05-30 21:16:23 -0300

    botones y cards seccion educacion

--- src/app/Componentes/educacion/educacion.component.ts
+++ src/app/Componentes/educacion/educacion.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.scss']
 })
-export class EducacionComponent {
+export class EducacionComponent implements OnInit{
+
+  botonactivado: string = "";
+
+  ngOnInit(): void {
+    this.botonactivado = "TODO";
+  }
+
+  setActivarBtn(boton: string){
+    console.log(boton);
+    this.botonactivado = boton;
+  }
+
+ 
 
 }
