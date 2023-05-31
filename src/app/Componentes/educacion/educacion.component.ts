d77e4efb5ca75636347d38a0e499bb3c0f98d5f7
GastonJava <gastonmirandajava@gmail.com>
2023-05-31 00:33:35 -0300

    fix botones de la seccion educacion y finalizando

--- src/app/Componentes/educacion/educacion.component.ts
+++ src/app/Componentes/educacion/educacion.component.ts
@@ -3,21 +3,30 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
-  styleUrls: ['./educacion.component.scss']
+  styleUrls: ['./educacion.component.scss'],
 })
-export class EducacionComponent implements OnInit{
+export class EducacionComponent implements OnInit {
+  botonactivado: string = '';
+  posicionFlecha: number = 0;
 
-  botonactivado: string = "";
+  offset: string = '0%';
 
   ngOnInit(): void {
-    this.botonactivado = "TODO";
+    this.botonactivado = 'TODO';
   }
 
-  setActivarBtn(boton: string){
+  setActivarBtn(boton: string) {
     console.log(boton);
     this.botonactivado = boton;
+
+    this.posicionFlecha = boton === 'TODO' ? 0 : 100; // Ajusta los valores de posición según tus necesidades
+
+    if (boton === 'TODO') {
+      this.offset = '0';
+    } else if (boton === 'CURSO') {
+      this.offset = '-50%';
+    } else if (boton === 'OTRO') {
+      this.offset = '50%';
+    }
   }
-
- 
-
 }
