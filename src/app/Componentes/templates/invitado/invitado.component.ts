9259e341da26c2b3148d9d141ed57bdf063c2ed3
GastonJava <gastonmirandajava@gmail.com>
2023-08-16 17:04:59 -0300

    agregamos animacion a las secciones al hacer scroll, se ejecutaran una sola vez

--- src/app/Componentes/templates/invitado/invitado.component.ts
+++ src/app/Componentes/templates/invitado/invitado.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-invitado',
@@ -7,9 +7,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvitadoComponent implements OnInit {
 
+  
+  @ViewChild('seccionheaderEl', { static: false }) seccionheaderEl!: ElementRef;
+  @ViewChild('seccionsobremiEl', { static: false }) seccionsobremiEl!: ElementRef;
+  @ViewChild('seccioneducacionEl', { static: false }) seccioneducacionEl!: ElementRef;
+  @ViewChild('seccionhardsoftEl', { static: false }) seccionhardsoftEl!: ElementRef;
+  @ViewChild('seccionproyectoEl', { static: false }) seccionproyectoEl!: ElementRef;
+  @ViewChild('seccionfooterEL', { static: false }) seccionfooterEL!: ElementRef;
+
+
+  //@ViewChild('animatedElement', { static: false }) animatedElement!: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
+   
+  }
+
+  ngAfterViewInit(){
+    this.ConfigurarIntersectionObserve(this.seccionheaderEl, "slide-in-left");
+    this.ConfigurarIntersectionObserve(this.seccionsobremiEl, "slide-in-right");
+    this.ConfigurarIntersectionObserve(this.seccioneducacionEl, "slide-in");
+    this.ConfigurarIntersectionObserve(this.seccionhardsoftEl, "slide-out");
+    this.ConfigurarIntersectionObserve(this.seccionproyectoEl, "slide-out");
+    this.ConfigurarIntersectionObserve(this.seccionfooterEL, "slide-out");
+
+  }
+
+  private ConfigurarIntersectionObserve (sectionRef: ElementRef, animacionClase: string) {
+
+    if(sectionRef){
+      const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.5 // Porcentaje de visibilidad para disparar la animación
+      };
+  
+      const observer = new IntersectionObserver(entries => {
+        console.log('Element is visible');
+  
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            // Agregar la clase de animación cuando el elemento esté en el viewport
+            sectionRef.nativeElement.classList.add(animacionClase);
+            observer.unobserve(entry.target);
+           //observer.disconnect();
+            
+          }
+          
+          /*else {
+            sectionRef.nativeElement.classList.remove(animacionClase);
+            
+          } */
+        });
+      }, options);
+  
+      // Observar el elemento deseado
+      observer.observe(sectionRef.nativeElement);
+    }
+    
   }
 
 }
--- src/app/Componentes/templates/invitado/sobre-mi/sobre-mi.component.ts
+++ src/app/Componentes/templates/invitado/sobre-mi/sobre-mi.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sobre-mi',
   templateUrl: './sobre-mi.component.html',
   styleUrls: ['./sobre-mi.component.scss']
 })
-export class SobreMiComponent {
+export class SobreMiComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+
 
 }
